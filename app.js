fcd64b14b57b684884827d62cd03993deb270889
Ahmed4549 <m.ahmedanis99@gmail.com>
2020-07-15 16:48:39 +0500

    last commit

--- app.js
+++ app.js
@@ -176,3 +176,27 @@
 // function alertBox(){
 //     alert("You clicked on a link")
 // }
+
+// Task # 2
+
+// function showAlertBox(){
+//     alert("Thanks For Shopping :)");
+// }
+
+// Task # 3
+
+// function deleteRow(r) {
+//     var i = r.parentNode.parentNode.rowIndex;
+//     document.getElementById("myTable").deleteRow(i);
+// }
+
+// Task # 4
+
+// function changeImage(id,src){
+//     var image = document.getElementById(id);
+//     image.src = src
+// }
+
+// Task # 5
+
+function increase()
