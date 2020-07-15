4aa72c994d1647b99da99d7b7b8e712dae80898e
Ahmed4549 <m.ahmedanis99@gmail.com>
2020-07-15 19:03:45 +0500

    JS_Assignment Chap-38_42

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // JavaScript
 
-// Chap # 38-44
+// Chap # 38-42
 
 // Task # 1 
 
@@ -168,35 +168,4 @@
 // }
 // notes()
 
-// --------------------------------------------------------------------------------
-// Chap # 43-48
-
-// Task # 1 
-
-// function alertBox(){
-//     alert("You clicked on a link")
-// }
-
-// Task # 2
-
-// function showAlertBox(){
-//     alert("Thanks For Shopping :)");
-// }
-
-// Task # 3
-
-// function deleteRow(r) {
-//     var i = r.parentNode.parentNode.rowIndex;
-//     document.getElementById("myTable").deleteRow(i);
-// }
-
-// Task # 4
-
-// function changeImage(id,src){
-//     var image = document.getElementById(id);
-//     image.src = src
-// }
-
-// Task # 5
-
-function increase()
+// ---END---
