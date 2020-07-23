4defc10c5a8c69c205c15489ea001d41e0913cc2
Ahmed4549 <m.ahmedanis99@gmail.com>
2020-07-23 17:38:22 +0500

    JS_Assignment Chap-53-58

--- app3.js
+++ app3.js
@@ -1,39 +1,52 @@
-// Get the modal
-var modal = document.getElementById("myModal");
-var modal2 = document.getElementById("myModal2");
+// Java Script Assignment
 
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-var btn2 = document.getElementById("myBtn2");
+// Chap 53-58
 
+// TAsk # 1
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-var span2 = document.getElementsByClassName("close2")[0];
+// var modal = document.getElementById("myModal");
+// var modal2 = document.getElementById("myModal2");
 
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-btn2.onclick = function() {
-    modal2.style.display = "block";
-  }
+// var btn = document.getElementById("myBtn");
+// var btn2 = document.getElementById("myBtn2");
 
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-span2.onclick = function() {
-    modal2.style.display = "none";
-  }
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-window.onclick = function(event) {
-    if (event.target == modal2) {
-      modal2.style.display = "none";
-    }
-  }
+// var span = document.getElementsByClassName("close")[0];
+// var span2 = document.getElementsByClassName("close2")[0];
+
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+// btn2.onclick = function() {
+//     modal2.style.display = "block";
+//   }
+
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+// span2.onclick = function() {
+//     modal2.style.display = "none";
+//   }
+
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
+// window.onclick = function(event) {
+//     if (event.target == modal2) {
+//       modal2.style.display = "none";
+//     }
+//   }
+
+// Task # 2
+
+// function Normal(){
+//   document.getElementsByTagName("p")[0].setAttribute("class", "NormalPara");
+// }
+// function ZoomIn(){
+//   document.getElementsByTagName("p")[0].setAttribute("class", "ZoomInPara");
+// }
+// function ZoomOut(){
+//   document.getElementsByTagName("p")[0].setAttribute("class", "ZoomOutPara");
+// }
